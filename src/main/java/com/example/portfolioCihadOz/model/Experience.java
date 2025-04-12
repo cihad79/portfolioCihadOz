369b198caf26fa9fcd47d1619305e0adb69df528
cihadozdemir <ciha0128@gmail.com>
2025-04-12 04:35:58 +0200

    new

--- src/main/java/com/example/portfolioCihadOz/model/Experience.java
+++ src/main/java/com/example/portfolioCihadOz/model/Experience.java
@@ -1,4 +1,3 @@
-
 package com.example.portfolioCihadOz.model;
 
 import jakarta.persistence.Entity;
--- src/main/java/com/example/portfolioCihadOz/services/ContactMessageService.java
+++ src/main/java/com/example/portfolioCihadOz/services/ContactMessageService.java
@@ -26,5 +26,4 @@ public class ContactMessageService {
         contactMessageRepository.deleteById(id);
     }
 
-
 }
