b26c7dff2a13b11a799083a82c2b089f66ea5303
cihadozdemir <ciha0128@gmail.com>
2025-04-14 03:05:20 +0200

    Configure database connection

--- src/main/java/com/example/portfolioCihadOz/PortfolioCihadOzApplication.java
+++ src/main/java/com/example/portfolioCihadOz/PortfolioCihadOzApplication.java
@@ -3,13 +3,10 @@ package com.example.portfolioCihadOz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class PortfolioCihadOzApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();  // Load the .env file
 
         SpringApplication.run(PortfolioCihadOzApplication.class, args);
     }
