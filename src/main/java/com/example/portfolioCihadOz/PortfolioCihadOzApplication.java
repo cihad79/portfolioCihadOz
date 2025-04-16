9da87acdb77bc2be7488e8b564b77261fb1f0815
cihadozdemir <ciha0128@gmail.com>
2025-04-16 17:22:03 +0200

    gooo gooo

--- src/main/java/com/example/portfolioCihadOz/PortfolioCihadOzApplication.java
+++ src/main/java/com/example/portfolioCihadOz/PortfolioCihadOzApplication.java
@@ -3,16 +3,10 @@ package com.example.portfolioCihadOz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class PortfolioCihadOzApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.out.println("JAWSDB_URL: " + dotenv.get("JAWSDB_URL"));
-        System.out.println("JAWSDB_USERNAME: " + dotenv.get("JAWSDB_USERNAME"));
-        System.out.println("JAWSDB_PASSWORD: " + dotenv.get("JAWSDB_PASSWORD"));
         SpringApplication.run(PortfolioCihadOzApplication.class, args);
     }
 
